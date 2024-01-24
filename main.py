4d4e6bd8b761ce3ca97e05aa92211a7ba0f4a2ee
STHassanDev <siakahassantraore@gmail.com>
2024-01-23 20:55:30 -0500

    Created the get stock price function

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import streamlit as st 
 import yfinance as yf 
+
+def get_stock_price(ticker): # Will be used when the prompt demands the stock price of a certain company
+    return str(yf.Ticker(ticker).history(period='1y').iloc[-1].Close)
+
